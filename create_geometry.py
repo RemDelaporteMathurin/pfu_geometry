0fc6e61cdde2dc5cb004f0ceaf9764c6c3dcd94f
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-31 14:44:04 +0000

    added plasma to reactor

--- create_geometry.py
+++ create_geometry.py
@@ -1,7 +1,8 @@
-from paramak import Reactor, Shape
+from paramak import Reactor, Shape, Plasma
 
 from pfu import my_pfu
 
+import os
 
 water = Shape(name="water")
 water.solid = my_pfu.water
@@ -20,7 +21,19 @@ for mb in my_pfu.monoblocks[1:]:
 cucrzr = Shape(name="cucrzr")
 cucrzr.solid = my_pfu.tube
 
-my_reactor = Reactor([water, tungsten, copper, cucrzr])
+
+plasma = Plasma(
+            major_radius=620,
+            minor_radius=200,
+            elongation=2,
+            triangularity=0.55,
+            rotation_angle=10
+        )
+
+my_reactor = Reactor([water, tungsten, copper, cucrzr, plasma])
+
+my_reactor.export_stl("reactor.stl")
+my_reactor.export_dagmc_h5m("dagmc.h5m")  # , exclude=["plasma"]
 
 
-my_reactor.export_dagmc_h5m("dagmc.h5m", exclude=["plasma"])
+os.system('mbconvert dagmc.h5m dagmc.vtk')
